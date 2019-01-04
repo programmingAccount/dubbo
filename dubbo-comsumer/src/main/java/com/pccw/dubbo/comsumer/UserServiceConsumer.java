f99ea6bc54eb7d20dbabc8f438178565d69ede00
programming <642981822@qq.com>
2019-01-04 17:06:20 +0800

    dubbodemo

---
+++ dubbo-comsumer/src/main/java/com/pccw/dubbo/comsumer/UserServiceConsumer.java
@@ -0,0 +1,19 @@
+package com.pccw.dubbo.comsumer;
+
+import com.pccw.dubbo.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UserServiceConsumer {
+    private static Logger logger = LoggerFactory.getLogger(UserServiceConsumer.class);
+
+    public static void main(String args[]) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        IUserService userService = (IUserService) ctx.getBean("userService");
+        logger.info("执行结果：" + userService.login("hello", "hello"));
+    }
+
+
+}
